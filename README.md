fac617823f2f1408fed32a78b96ec1a6e4a12835
Jim Laney <jlaney@uw.edu>
2024-02-22 08:49:12 -0800

    add python test matrix

--- README.md
+++ README.md
@@ -3,4 +3,27 @@
 [![Build Status](https://github.com/uw-it-aca/uw-restclients-kws/workflows/tests/badge.svg?branch=main)](https://github.com/uw-it-aca/uw-restclients-kws/actions)
 [![Coverage Status](https://coveralls.io/repos/github/uw-it-aca/uw-restclients-kws/badge.svg?branch=main)](https://coveralls.io/github/uw-it-aca/uw-restclients-kws?branch=main)
 [![PyPi Version](https://img.shields.io/pypi/v/uw-restclients-kws.svg)](https://pypi.python.org/pypi/uw-restclients-kws)
-![Python versions](https://img.shields.io/pypi/pyversions/uw-restclients-kws.svg)
+![Python versions](https://img.shields.io/badge/python-3.10-blue.svg)
+
+Installation:
+
+    pip install UW-RestClients-KWS
+
+To use this client, you'll need these settings in your application or script:
+
+    # Specifies whether requests should use live or mocked resources,
+    # acceptable values are 'Live' or 'Mock' (default)
+    RESTCLIENTS_KWS_DAO_CLASS='Live'
+
+    # Paths to cert and key files
+    RESTCLIENTS_KWS_CERT_FILE='/path/to/cert'
+    RESTCLIENTS_KWS_KEY_FILE='/path/to/key'
+
+    # Key Web Service hostname (eval or production)
+    RESTCLIENTS_KWS_HOST='https://ws.admin.washington.edu'
+
+Optional settings:
+
+    # Customizable parameters for urllib3
+    RESTCLIENTS_KWS_TIMEOUT=5
+    RESTCLIENTS_KWS_POOL_SIZE=10
--- uw_kws/tests/test_key.py
+++ uw_kws/tests/test_key.py
@@ -1,4 +1,4 @@
-# Copyright 2021 UW-IT, University of Washington
+# Copyright 2024 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
 from unittest import TestCase
@@ -13,17 +13,17 @@ class KWSTestKeyData(TestCase):
     def test_key_by_id(self):
         kws = KWS()
         key = kws.get_key('ee99defd-baee-43b0-9e1e-f8238dd106bb')
-        self.assertEquals(key.algorithm, 'AES128CBC', 'Correct algorithm')
-        self.assertEquals(key.cipher_mode, 'CBC', 'Correct cipher mode')
-        self.assertEquals(
+        self.assertEqual(key.algorithm, 'AES128CBC', 'Correct algorithm')
+        self.assertEqual(key.cipher_mode, 'CBC', 'Correct cipher mode')
+        self.assertEqual(
             key.expiration.isoformat(),
             '2013-04-11T13:44:33.360000', 'Correct expiration')
-        self.assertEquals(
+        self.assertEqual(
             key.key_id,
             'ee99defd-baee-43b0-9e1e-f8238dd106bb', 'Correct key ID')
-        self.assertEquals(key.key, 'Uv2JsxggfxF9OQNzIxAzDQ==', 'Correct key')
-        self.assertEquals(key.size, 128, 'Correct key size')
-        self.assertEquals(key.url, (
+        self.assertEqual(key.key, 'Uv2JsxggfxF9OQNzIxAzDQ==', 'Correct key')
+        self.assertEqual(key.size, 128, 'Correct key size')
+        self.assertEqual(key.url, (
             'https://it-wseval1.s.uw.edu/key/v1/encryption/'
             'ee99defd-baee-43b0-9e1e-f8238dd106bb.json'), 'Correct key URL')
 
@@ -45,16 +45,16 @@ class KWSTestKeyData(TestCase):
     def test_current_key(self):
         kws = KWS()
         key = kws.get_current_key('uw-student-registration')
-        self.assertEquals(key.algorithm, 'AES128CBC', 'Correct algorithm')
-        self.assertEquals(key.cipher_mode, 'CBC', 'Correct cipher mode')
-        self.assertEquals(
+        self.assertEqual(key.algorithm, 'AES128CBC', 'Correct algorithm')
+        self.assertEqual(key.cipher_mode, 'CBC', 'Correct cipher mode')
+        self.assertEqual(
             key.expiration.isoformat(),
             '2013-04-11T13:44:33.360000', 'Correct expiration')
-        self.assertEquals(
+        self.assertEqual(
             key.key_id,
             'ee99defd-baee-43b0-9e1e-f8238dd106bb', 'Correct key ID')
-        self.assertEquals(key.key, 'Uv2JsxggfxF9OQNzIxAzDQ==', 'Correct key')
-        self.assertEquals(key.size, 128, 'Correct key size')
-        self.assertEquals(key.url, (
+        self.assertEqual(key.key, 'Uv2JsxggfxF9OQNzIxAzDQ==', 'Correct key')
+        self.assertEqual(key.size, 128, 'Correct key size')
+        self.assertEqual(key.url, (
             'https://it-wseval1.s.uw.edu/key/v1/encryption/'
             'ee99defd-baee-43b0-9e1e-f8238dd106bb.json'), 'Correct key URL')
